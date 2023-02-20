2566f6d166018c879baaf843cfe14c7c4224f19d
Easinal <wan_zijin@163.com>
2023-02-19 18:24:27 -0800

    change init_seq

--- init_seq.cpp
+++ init_seq.cpp
@@ -1,8 +1,3 @@
-#include <iostream>
-#include <parlay/sequence.h>
-using namespace std;
-using namespace parlay;
-
 int initializeRandomWeight(sequence<size_t> &arr, size_t size, size_t limit, size_t seed = 0.01){
   parlay::parallel_for(0, size, [&](size_t i){
     arr[i]=hash64(i+seed*size)%limit;
