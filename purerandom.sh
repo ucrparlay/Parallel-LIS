08b8536f7cbc5a10a0dbeba91c75e0b57262723d
Zijin Wan <48508430+Easinal@users.noreply.github.com>
2023-04-20 18:02:37 -0700

    Update purerandom.sh

--- purerandom.sh
+++ purerandom.sh
@@ -2,13 +2,11 @@ make clean
 make
 
 folder="result1"
-#folder="/data0/zwan018/Parallel-LIS/pureRandom/"
 round="10"
 limit=1000000000
 sizes=("1000000" "10000000" "100000000" "1000000000")
-cores=("2" "4" "8" "16" "32" )
-#"64" "128" "192"
-# 
+cores=("2" "4" "8" "16" "32"  "64" "128" "192"）
+
 echo "" > $folder/LIS_PureRandom.log
 for size in ${sizes[@]}; do
     echo ${size} > $folder/c.txt
